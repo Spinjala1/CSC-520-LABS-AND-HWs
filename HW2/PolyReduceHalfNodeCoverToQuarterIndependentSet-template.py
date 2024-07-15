08b420b967497b2378608bfc68090ee76ad93e56
SKTC4444 <106710603+SKTC4444@users.noreply.github.com>
2024-07-14 22:55:41 -0700

    Added HW2 solutions

--- HW2/PolyReduceHalfNodeCoverToQuarterIndependentSet-template.py
+++ HW2/PolyReduceHalfNodeCoverToQuarterIndependentSet-template.py
@@ -1,18 +1,26 @@
+# On our honor, as SFSU students, we, Sripranav Pinjala, Annison Van,
+# John King, and Subhan Khan, did not give or receive inappropriate help
+# with this assignment. All group members contributed to this work, and
+# all concur with the submission. We understand that we will be asked to
+# redo the assignment in person if this work presents any question of an
+# honor code violation.
+
 from graph import Graph
 from VfyQuarterIndependentSet import VfyQuarterIndependentSet
 
-HNC2QISv =  'VERBOSE: PolyReduceHalfNodeCoverToQuarterIndpenedentSet() - '
+HNC2QISv = 'VERBOSE: PolyReduceHalfNodeCoverToQuarterIndpenedentSet() - '
 VERBOSE = True
+
 def printV(text):
     if VERBOSE: print(f'{HNC2QISv}{text}')
 
-HNC2QISd =  'DEV:PolyReduceHalfNodeCoverToQuarterIndpenedentSet() - '
+HNC2QISd = 'DEV:PolyReduceHalfNodeCoverToQuarterIndpenedentSet() - '
 DEV = False
+
 def printD(text):
     if DEV: print(f'{HNC2QISd}{text}')
 
-
-def make_new_node(nodes,new_nodes):
+def make_new_node(nodes, new_nodes):
     '''
     Arguments:
       nodes -- list of nodes in the halfNodeCover instance
@@ -21,15 +29,12 @@ def make_new_node(nodes,new_nodes):
     Returns a node name consisting of Z's, which is not in either
     nodes or new_nodes
     '''
-    
     new_node = 'Z'
     names = nodes + new_nodes
-    for n in names:
-        if n == new_node: new_node += 'Z'
-        
+    while new_node in names:
+        new_node += 'Z'
     return new_node
 
-    
 def PolyReduceHalfNodeCoverToQuarterIndependentSet(halfNodeCover_instance):
     graph_str = halfNodeCover_instance
 
@@ -37,93 +42,72 @@ def PolyReduceHalfNodeCoverToQuarterIndependentSet(halfNodeCover_instance):
     nodes = list(g.nodes.keys())
 
     new_nodes = []
-    
-    ### HW #2: Create the correct number of new node names,
-    ###             making sure that they're unique
-    #
-    nodes.extend(new_nodes) ## Add the new nodes to the old ones
+    for _ in nodes:
+        new_nodes.append(make_new_node(nodes, new_nodes))
 
-    ### HW #2: Create new edges to connect the new nodes
-    ###             to the graph in the correct way. Remember,
-    ###             in an undirected graph, the edge 'a,b' is
-    ###             the same as the edge 'b,a', and including
-    ###             both in a graph would incur a WCBC error from
-    ###             graph.py
-    #
+    nodes.extend(new_nodes)
+
     new_edges = []
+    for new_node in new_nodes:
+        for old_node in g.nodes.keys():
+            new_edges.append(f'{new_node},{old_node}')
 
-    ### HW #2: Use your work above to make the needed transformation
-    ###            of halfNodeCover_instance into quarterIndependentSet_instance,
-    ###            such that quarterIndependentSet_instance will be a positive
-    ###            instance of QuarterIndependentSet iff halfNodeCover_instance
-    ###            is a positive instance of HalfNodeCover.  Making no change, as
-    ###            in the code below that you are asked to change, could result in
-    ###            mapping a negative instance of HalfNodeCover onto a positive
-    ###            instance of QuarterIndependent set.
-    #
-    quarterIndependentSet_instance = halfNodeCover_instance ## ** CHANGE THIS **
-    
+
+    existing_edges = [f'{edge[0]},{edge[1]}' for edge in g.edges()]
+
+    quarterIndependentSet_instance = ' '.join(existing_edges + new_edges)
+
     printV(f'quarterIndependentSet_instance = "{quarterIndependentSet_instance}"')
-    
+
     return quarterIndependentSet_instance
-C =  PolyReduceHalfNodeCoverToQuarterIndependentSet
 
-
-def vfyHalfNodeCoverViaVfyQuarterIndepedentSet(halfNodeCover_instance,S,H):
-    quarterIndependentSet_instance = C(halfNodeCover_instance)
+def vfyHalfNodeCoverViaVfyQuarterIndepedentSet(halfNodeCover_instance, S, H):
+    quarterIndependentSet_instance = PolyReduceHalfNodeCoverToQuarterIndependentSet(halfNodeCover_instance)
     graph_str = halfNodeCover_instance
 
     g = Graph(graph_str, weighted=False, directed=False)
-    nodes = list(g.nodes.keys()) #Convert to list
-    
-    # The hint for quarterIndependentSet consists of whatever nodes are not
-    # in the hint for halfnodeCover
-    # 
+    nodes = list(g.nodes.keys())
+
     printD(f'nodes = {nodes}, H = {H.split()}')
     H_independent_set = ' '.join([node for node in nodes if node not in H.split()])
 
     printD('Hint for Quarter Independent Set verifier to use on converted' + \
            f' instance: "{H_independent_set}":\n ' + \
            f'        (nodes in node cover graph instance, but not in H = "{H}")')
-    result = \
-        VfyQuarterIndependentSet(quarterIndependentSet_instance,S,H_independent_set)
+    result = VfyQuarterIndependentSet(quarterIndependentSet_instance, S, H_independent_set)
     return result
 
-
 if __name__ == '__main__':
-
-    def test_case(F,I,S,H,expected,num,comment=''):
+    def test_case(F, I, S, H, expected, num, comment=''):
         err = '** '
-        result = F(I,S,H)
+        result = F(I, S, H)
         func_name = str(F).split()[1]
         func_call = f'''{func_name}("{I}","{S}","{H}")'''
         if result == expected: err = ''
         e = expected
-        print (f'{err}test #{num} {func_call}: expected "{e}", received "{result}"')
-        print (f'test #{num} Explanation: {comment}\n')
+        print(f'{err}test #{num} {func_call}: expected "{e}", received "{result}"')
+        print(f'test #{num} Explanation: {comment}\n')
         return num + 1
 
     F = vfyHalfNodeCoverViaVfyQuarterIndepedentSet
 
-
     num = 1
-    I = 'a,b  a,c a,d'                    
+    I = 'a,b a,c a,d'
     exp = '{a} covers all 4 nodes'
-    num = test_case(F,I,'yes','a','correct',num,exp)
+    num = test_case(F, I, 'yes', 'a', 'correct', num, exp)
 
-    I = 'a,b  a,c c,d'                    
+    I = 'a,b a,c c,d'
     exp = '{a,c} covers all 4 nodes'
-    num = test_case(F,I,'yes','a c','correct',num,exp)
+    num = test_case(F, I, 'yes', 'a c', 'correct', num, exp)
 
-    I = 'a,b  b,c c,d e,f'                    
-    Exp = '{a,c,e} is half of 6 nodes'
-    num = test_case(F,I,'yes','a c e','correct',num,exp)
+    I = 'a,b b,c c,d e,f'
+    exp = '{a,c,e} is half of 6 nodes'
+    num = test_case(F, I, 'yes', 'a c e', 'correct', num, exp)
 
-    I = 'a,b  a,c b,d'                    
+    I = 'a,b a,c b,d'
     exp = '{a,c} does not cover nodes'
-    num = test_case(F,I,'yes','a c','unsure',num,exp)
+    num = test_case(F, I, 'yes', 'a c', 'unsure', num, exp)
 
-    I = 'a,b a,c a,d b,c c,e'                    
+    I = 'a,b a,c a,d b,c c,e'
     exp = '{a,b,c} is over half of 5 nodes'
-    num = test_case(F,I,'yes',' a b c','unsure',num,exp)
-
+    num = test_case(F, I, 'yes', ' a b c', 'unsure', num, exp)
--- HW2/VfyStartEndSameColor-template.py
+++ HW2/VfyStartEndSameColor-template.py
@@ -1,3 +1,10 @@
+# On our honor, as SFSU students, we, Sripranav Pinjala, Annison Van,
+# John King, and Subhan Khan, did not give or receive inappropriate help
+# with this assignment. All group members contributed to this work, and
+# all concur with the submission. We understand that we will be asked to
+# redo the assignment in person if this work presents any question of an
+# honor code violation.
+
 from graph import Graph
 
 # identifier for verbose output
@@ -12,104 +19,96 @@ def printD(text):
     if DEV: print(f'{VSESCd}{text}')
 
 def VfyStartEndSameColor(I, S, H):
-    if len(S) > 3 or len(H) > 5 * len(I): 
-        printV(f'unreasonable length hint or solution')
+    if len(S) > 3 or len(H) > 5 * len(I):
+        printV('unreasonable length hint or solution')
         return 'unsure'
 
     if S != 'yes':
-        printV(f'solution != "yes"')
+        printV('solution != "yes"')
         return 'unsure'
-        
+
     cycle = H.split(',')  # Hint is a comma delimited list of nodes
     if len(cycle) < 2:
-        printV(f'Cycles must have at least 2 nodes.')
+        printV('Cycles must have at least 2 nodes.')
         return 'unsure'
 
     instance_graph, instance_colorings = I.split(';')
 
-    # Use WCBC library function to create a directed, unweighted graph
+
     g = Graph(instance_graph, directed=True, weighted=False)
 
-    nodes = list(g.nodes)
-    nodes_sav = nodes[:]  # clone node list for future reference
-    edges = list(g.edges())
-    
-    for idx in range(len(edges)):
-        # convert edges from a list of graph edges to a list of strings
-        edges[idx] = str(edges[idx])    
-    
-    colors = []         # colors 
     node_color_kv = {}  # node->color key/value pairs
     color_count_kv = {} # color->count key/value pairs
     colorings_list = instance_colorings.split()
     for node_color in colorings_list:
         node, color = node_color.split(':')
         node_color_kv[node] = color
-        if color not in colors:
-            colors.append(color)
+        if color not in color_count_kv:
+            color_count_kv[color] = 0
 
-        color_count_kv[color] = 0
     s_node = cycle[0]
-    s_color = None
     try:
         s_color = node_color_kv[s_node]
-    except:
+    except KeyError:
         printV(f'"{s_node}" in hint but not graph')
         return 'unsure'
 
     e_node = cycle[-1]
-    e_color = None
     try:
         e_color = node_color_kv[e_node]
-    except:
+    except KeyError:
         printV(f'"{e_node}" in hint but not graph')
         return 'unsure'
-     
-    e_color = node_color_kv[cycle[-1]]
+
     if s_color != e_color:
         printV(f'{s_color} starts cycle and {e_color} ends it.')
         return 'unsure'
 
-    ### ** HW #2 Add any and all checks needed for verification failures
-    # Ensure all nodes in cycle exist in the graph
-    for node in cycle:
-        if node not in node_color_kv:
-            printV(f'"{node}" in hint but not graph')
-            return 'unsure'
-
-    # Ensure there are no duplicate colors except for the start/end node
+    # makes sure there are no duplicate colors except for the start/end node
     seen_colors = set()
-    for i in range(len(cycle)):
+    for i in range(1, len(cycle) - 1):
         color = node_color_kv[cycle[i]]
-        if i == 0 or i == len(cycle) - 1:
-            continue
         if color in seen_colors:
             printV(f'duplicate color {color} found in cycle')
             return 'unsure'
         seen_colors.add(color)
 
-    # Ensure the cycle contains each color exactly once, except the start/end color
-    cycle_colors = [node_color_kv[node] for node in cycle]
-    if len(set(cycle_colors)) != len(cycle_colors) - 1:
-        printV(f'Cycle does not contain each color exactly once except start/end color')
+        #  Makes sure all nodes in cycle exist in the graph
+    for node in node_color_kv:
+        if node not in cycle:
+            printV(f'"{node_color_kv[node]}" not in cycle')
+            return 'unsure'
+
+    # Makes sure the cycle contains each color exactly once, except the start/end color
+    cycle_colors = [node_color_kv[node] for node in cycle[1:-1]]
+    if len(cycle_colors) != len(set(cycle_colors)):
+        printV('Cycle does not contain each color exactly once except start/end color')
         return 'unsure'
-    
-    # Ensure all edges in the cycle exist in the graph
+
+    # makes sure all edges in the cycle exist in the graph
     for i in range(len(cycle)):
+        newedges = str(list(g.edges()))
+
         if i == len(cycle) - 1:
-            if (cycle[i], cycle[0]) not in g.edges():
-                printV(f'Edge ({cycle[i]}, {cycle[0]}) not in graph')
+            templist = [cycle[i], ',', cycle[0]]
+            temp = ''.join(templist)
+            if temp not in newedges:
+                printV(f'Edge ({cycle[i]},{cycle[0]}) not in graph')
                 return 'unsure'
         else:
-            if (cycle[i], cycle[i+1]) not in g.edges():
-                printV(f'Edge ({cycle[i]}, {cycle[i+1]}) not in graph')
+            templist = [cycle[i], ',', cycle[i+1]]
+            temp = ''.join(templist)
+            if temp not in newedges:
+                printV(f'Edge ({cycle[i]},{cycle[i+1]}) not in graph')
                 return 'unsure'
+
 
     printV(f'"{I}" is a positive instance, all verifications succeeded')
     return 'correct'
 
 if __name__ == '__main__':
     def test_case(F, I, S, H, expected, num, comment=''):
+
         # Evaluate test case to see whether or not it meets expectations.
         err = '** '   # Error flag is on by default
         result = F(I, S, H) # Call the verifier and store the result.
